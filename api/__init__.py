3fedb8e7ea9a51c4a0c0f8b535e54dbf90da18a4
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2025-09-02 19:46:11 -0300

    feat: remove integração com o arduino

--- api/__init__.py
+++ api/__init__.py
@@ -3,13 +3,10 @@ from flask_restful import Api
 from flask_pymongo import PyMongo
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
-from .serial_controller import init_arduino
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = 'mongodb://localhost:27017/learny-bd'
 app.config["JWT_SECRET_KEY"] = "super-secret-key"  # chave secreta
-
-init_arduino('COM6')
 
 jwt = JWTManager(app)
 ma = Marshmallow(app)
@@ -18,5 +15,5 @@ api = Api(app)
 mongo = PyMongo(app)
 
 # Importando os recursos
-from .resources import parents_resources, children_resources, login_resources, button_resources
+from .resources import parents_resources, children_resources, login_resources
 
